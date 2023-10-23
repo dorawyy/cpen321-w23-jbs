3778256fd37f43e02c4cd63409cbd6b86b15326c
aryaphan <aryaphan31@gmail.com>
2023-10-23 11:54:13 -0700

    moved recommendation routes into one function

--- backend/routes/recommendation.routes.js
+++ backend/routes/recommendation.routes.js
@@ -2,25 +2,27 @@ const controller = require("../controllers/recommendation.controller")
 const { authJwt, verifySignUp } = require("../middleware")
 
 module.exports = function (app) {
-    app.post("/user_action/checked_profile",
+    app.post(
+        "/user_action/checked_profile",
         authJwt.verifyJwt,
-        controller.checkedProfile);
-};
-
-module.exports = function (app) {
-    app.post("/user_action/contacted_tutor",
+        controller.checkedProfile
+    );
+    
+    app.post(
+        "/user_action/contacted_tutor",
         authJwt.verifyJwt,
-        controller.contactedTutor);
-};
-
-module.exports = function (app) {
-    app.post("/user_action/scheduled_appointment",
+        controller.contactedTutor
+    );
+    
+    app.post(
+        "/user_action/scheduled_appointment",
         authJwt.verifyJwt,
-        controller.scheduledAppointment);
-};
-
-module.exports = function (app) {
-    app.post("/user_action/reviewed_tutor",
+        controller.scheduledAppointment
+    );
+    
+    app.post(
+        "/user_action/reviewed_tutor",
         authJwt.verifyJwt,
-        controller.reviewedTutor);
-};
+        controller.reviewedTutor
+    );
+}

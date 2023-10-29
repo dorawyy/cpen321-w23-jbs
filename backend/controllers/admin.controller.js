eb4f9ad14cdeddfbdaf0db70d8a641c17dd0de8c
StanDarJeep <stzhao2@gmail.com>
2023-10-29 16:59:03 -0700

    fix admin profile bug and add messages

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -3,6 +3,7 @@ const { UserType } = require("../constants/user.types");
 const mongoose = require('mongoose')
 
 const User = db.user
+const Conversation = db.conversation
 
 exports.ban = async (req, res) => {
     const admin = await User.findById(req.userId)
@@ -71,10 +72,10 @@ exports.getProfile = async (req, res) => {
     if (admin.type != UserType.ADMIN)
         res.status(401).send({ message: "User is not admin and is not authorized to view user profile and messages" })
 
-    if (!mongoose.Types.ObjectId.isValid(req.body.userId)) {
+    if (!mongoose.Types.ObjectId.isValid(req.query.userId)) {
         return res.status(400).send({ message: "Invalid provided userId" })
     }
-    const user = await User.findById(req.body.userId)
+    const user = await User.findById(req.query.userId)
 
     if (!user) {
         return res.status(400).send({ message: "Could not find user in database with provided id" })
@@ -82,17 +83,32 @@ exports.getProfile = async (req, res) => {
 
     const userReviews = await User.aggregate([
         { $unwind: '$userReviews' },
-        { $match: { 'userReviews.reviewerId': req.body.userId } },
+        { $match: { 'userReviews.reviewerId': req.query.userId } },
         { $project: { 
             '_id': 0,
             'comment': 'userReviews.comment'
         } }
     ])
 
-    return res.status(200).json({
-        bio: user.bio,
-        reviews: userReviews
+    const userConversations = await Conversation.find({
+        $or: [
+            { 'participants.userId1': req.query.userId },
+            { 'participants.userId2': req.query.userId }
+        ]
     })
 
-    // TODO: add conversation return in res
+    const userMessages = []
+    userConversations.forEach(conversation => {
+        const filteredMessages = conversation.messages.filter(message => {
+            message.senderId === req.query.userId
+        })
+    
+        userMessages.push(...filteredMessages)
+    })
+
+    return res.status(200).json({
+        bio: user.bio,
+        reviews: userReviews,
+        messages: userMessages
+    })
 }

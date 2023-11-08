88d1f8b987bf1b7d7e45cdded9c9b74869e73564
StanDarJeep <stzhao2@gmail.com>
2023-11-08 13:54:08 -0800

    fix recommendation bug

--- backend/controllers/recommendation.controller.js
+++ backend/controllers/recommendation.controller.js
@@ -24,10 +24,10 @@ exports.checkedProfile = async (req, res) => {
         if (!tutee || tutee.isBanned)
             return res.status(404).send({ message: "Could not find tutee in database with provided id"})
     
-        if (tutor.rating) {
+        if (tutor.rating)
             tutee.recommendationWeights.minRating -= (tutee.recommendationWeights.minRating - tutor.rating) * (tutor.rating < tutee.recommendationWeights.minRating ? 0.15 : 0.05)
-            tutee.recommendationWeights.locationModeWeight -= (tutor.locationMode != tutee.locationMode ? 0.01 : -0.005)
-        }
+
+        tutee.recommendationWeights.locationModeWeight -= (tutor.locationMode != tutee.locationMode ? 0.01 : -0.005)
     
         if (tutee.locationMode == LocationMode.IN_PERSON && tutor.location) {
             const distance = haversine({ latitude: tutee.location.lat, longitude: tutee.location.long}, { latitude: tutor.location.lat, longitude: tutor.location.long})
@@ -69,10 +69,10 @@ exports.contactedTutor = async (req, res) => {
             tutee.recommendationWeights.budget += (averageHourlyRate - tutee.recommendationWeights.budget) * (averageHourlyRate > tutee.recommendationWeights.budget ? 0.15 : 0.05)
         }
     
-        if (tutor.rating) {
+        if (tutor.rating)
             tutee.recommendationWeights.minRating -= (tutee.recommendationWeights.minRating - tutor.rating) * (tutor.rating < tutee.recommendationWeights.minRating ? 0.3 : 0.05)
-            tutee.recommendationWeights.locationModeWeight -= (tutor.locationMode != tutee.locationMode ? 0.05 : -0.01)
-        }
+        
+        tutee.recommendationWeights.locationModeWeight -= (tutor.locationMode != tutee.locationMode ? 0.05 : -0.01)
     
         if (tutee.locationMode == LocationMode.IN_PERSON && tutor.location) {
             const distance = haversine({ latitude: tutee.location.lat, longitude: tutee.location.long}, { latitude: tutor.location.lat, longitude: tutor.location.long})
@@ -120,10 +120,10 @@ exports.scheduledAppointment = async (req, res) => {
             tutee.recommendationWeights.budget) * 
             (scheduledSubjectHourlyRate.hourlyRate > tutee.recommendationWeights.budget ? 0.5 : 0.1)
     
-        if (tutor.rating) {
+        if (tutor.rating)
             tutee.recommendationWeights.minRating -= (tutee.recommendationWeights.minRating - tutor.rating) * (tutor.rating < tutee.recommendationWeights.minRating ? 0.5 : 0.1)
-            tutee.recommendationWeights.locationModeWeight -= (tutor.locationMode != tutee.locationMode ? 0.1 : -0.02)
-        }
+
+        tutee.recommendationWeights.locationModeWeight -= (tutor.locationMode != tutee.locationMode ? 0.1 : -0.02)
     
         if (tutee.locationMode == LocationMode.IN_PERSON && tutor.location) {
             const distance = haversine({ latitude: tutee.location.lat, longitude: tutee.location.long}, { latitude: tutor.location.lat, longitude: tutor.location.long})
@@ -167,11 +167,11 @@ exports.reviewedTutor = async (req, res) => {
             tutee.recommendationWeights.budget += (averageHourlyRate - tutee.recommendationWeights.budget) * (averageHourlyRate > tutee.recommendationWeights.budget ? 0.15 : 0.05) * reviewFactor
         }
     
-        if (tutor.rating) {
+        if (tutor.rating)
             tutee.recommendationWeights.minRating -= (tutee.recommendationWeights.minRating - tutor.rating) * (tutor.rating < tutee.recommendationWeights.minRating ? 0.3 : 0.05) * reviewFactor
-            tutee.recommendationWeights.locationModeWeight -= (tutor.locationMode != tutee.locationMode ? 0.05 : -0.01) * reviewFactor
-        }
         
+        tutee.recommendationWeights.locationModeWeight -= (tutor.locationMode != tutee.locationMode ? 0.05 : -0.01) * reviewFactor
+
         if (tutee.locationMode == LocationMode.IN_PERSON && tutor.location) {
             const distance = haversine({ latitude: tutee.location.lat, longitude: tutee.location.long}, { latitude: tutor.location.lat, longitude: tutor.location.long})
             tutee.recommendationWeights.maxDistance += (distance - tutee.recommendationWeights.maxDistance) * (distance > tutee.recommendationWeights.maxDistance ? 0.2 : 0.02) * reviewFactor

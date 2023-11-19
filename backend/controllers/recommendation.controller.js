83af153d3aa2ba34f2932bf8f8c0e3c1fc2b7517
Stanley Zhao <stzhao2@gmail.com>
2023-11-18 21:33:37 -0800

    improve recommendation error handling and add tests

--- backend/controllers/recommendation.controller.js
+++ backend/controllers/recommendation.controller.js
@@ -1,34 +1,22 @@
 const db = require("../db")
 const { LocationMode } = require("../constants/location.modes");
 const haversine = require('haversine')
-const mongoose = require('mongoose')
 
 const User = db.user
 
 // ChatGPT usage: No
 exports.checkedProfile = async (req, res) => {
-    console.log("")
     try {
-        if (!mongoose.Types.ObjectId.isValid(req.body.tutorId)) {
-            return res.status(400).send({ message: "Invalid provided tutorId" })
-        }
-        const tutor = await User.findById(req.body.tutorId).catch(err => {
-            console.log(err)
-            return res.status(500).send({ message: err.message })
-        })
-        const tutee = await User.findById(req.userId).catch(err => {
-            console.log(err)
-            return res.status(500).send({ message: err.message })
-        })
+        const tutor = await User.findById(req.body.tutorId)
+        const tutee = await User.findById(req.userId)
+    
         if (!tutor || tutor.isBanned)
             return res.status(404).send({ message: "Could not find tutor in database with provided id"})
-        if (!tutee || tutee.isBanned)
-            return res.status(404).send({ message: "Could not find tutee in database with provided id"})
     
-        if (tutor.rating)
-            tutee.recommendationWeights.minRating -= (tutee.recommendationWeights.minRating - tutor.rating) *
-                (tutor.rating < tutee.recommendationWeights.minRating ? 0.15 : 0.05)
-
+        if (tutor.overallRating)
+            tutee.recommendationWeights.minRating -= (tutee.recommendationWeights.minRating - tutor.overallRating) *
+                (tutor.overallRating < tutee.recommendationWeights.minRating ? 0.15 : 0.05)
+    
         tutee.recommendationWeights.locationModeWeight -= (tutor.locationMode != tutee.locationMode ?
             0.01 :
             -0.005
@@ -41,10 +29,7 @@ exports.checkedProfile = async (req, res) => {
                 (distance > tutee.recommendationWeights.maxDistance ? 0.15 : 0.02)
         }
     
-        await tutee.save().catch(err => {
-            console.log(err)
-            return res.status(500).send({ message: err.message })
-        })
+        await tutee.save()
         return res.status(200).send({ message: "Adjusted weights based on checked profile"})
     } catch (err) {
         console.log(err)
@@ -55,49 +40,36 @@ exports.checkedProfile = async (req, res) => {
 // ChatGPT usage: No
 exports.contactedTutor = async (req, res) => {
     try {
-        if (!mongoose.Types.ObjectId.isValid(req.body.tutorId)) {
-            return res.status(400).send({ message: "Invalid provided tutorId" })
-        }
-        const tutor = await User.findById(req.body.tutorId).catch(err => {
-            console.log(err)
-            return res.status(500).send({ message: err.message })
-        })
-        const tutee = await User.findById(req.userId).catch(err => {
-            console.log(err)
-            return res.status(500).send({ message: err.message })
-        })
+        const tutor = await User.findById(req.body.tutorId)
+        const tutee = await User.findById(req.userId)
+
         if (!tutor || tutor.isBanned)
             return res.status(404).send({ message: "Could not find tutor in database with provided id"})
-        if (!tutee || tutee.isBanned)
-            return res.status(404).send({ message: "Could not find tutee in database with provided id"})
-    
-        if (!tutor.subjectHourlyRate || tutor.subjectHourlyRate.length !== 0) {
+
+        if (tutor.subjectHourlyRate && tutor.subjectHourlyRate.length !== 0) {
             const averageHourlyRate = tutor.subjectHourlyRate.reduce((acc, subject) =>
                 acc + subject.hourlyRate, 0) / tutor.subjectHourlyRate.length
             tutee.recommendationWeights.budget += (averageHourlyRate - tutee.recommendationWeights.budget) *
                 (averageHourlyRate > tutee.recommendationWeights.budget ? 0.15 : 0.05)
         }
-    
-        if (tutor.rating)
-            tutee.recommendationWeights.minRating -= (tutee.recommendationWeights.minRating - tutor.rating) *
-                (tutor.rating < tutee.recommendationWeights.minRating ? 0.3 :0.05)
+
+        if (tutor.overallRating)
+            tutee.recommendationWeights.minRating -= (tutee.recommendationWeights.minRating - tutor.overallRating) *
+                (tutor.overallRating < tutee.recommendationWeights.minRating ? 0.3 :0.05)
         
         tutee.recommendationWeights.locationModeWeight -= (tutor.locationMode != tutee.locationMode ?
             0.05 :
             -0.01
         )
-    
+
         if (tutee.locationMode == LocationMode.IN_PERSON && tutor.location) {
             const distance = haversine({ latitude: tutee.location.lat, longitude: tutee.location.long },
                 { latitude: tutor.location.lat, longitude: tutor.location.long })
             tutee.recommendationWeights.maxDistance += (distance - tutee.recommendationWeights.maxDistance) *
                 (distance > tutee.recommendationWeights.maxDistance ? 0.2 : 0.02)
         }
-    
-        await tutee.save().catch(err => {
-            console.log(err)
-            return res.status(500).send({ message: err.message })
-        })
+
+        await tutee.save()
         return res.status(200).send({ message: "Adjusted weights based on contacted tutor"})
     } catch (err) {
         console.log(err)
@@ -108,37 +80,27 @@ exports.contactedTutor = async (req, res) => {
 // ChatGPT usage: No
 exports.scheduledAppointment = async (req, res) => {
     try {
-        if (!mongoose.Types.ObjectId.isValid(req.body.tutorId)) {
-            return res.status(400).send({ message: "Invalid provided tutorId" })
-        }
-        const tutor = await User.findById(req.body.tutorId).catch(err => {
-            console.log(err)
-            return res.status(500).send({ message: err.message })
-        })
-        const tutee = await User.findById(req.userId).catch(err => {
-            console.log(err)
-            return res.status(500).send({ message: err.message })
-        })
+        const tutor = await User.findById(req.body.tutorId)
+        const tutee = await User.findById(req.userId)
+    
         if (!tutor || tutor.isBanned)
             return res.status(404).send({ message: "Could not find tutor in database with provided id"})
-        if (!tutee || tutee.isBanned)
-            return res.status(404).send({ message: "Could not find tutee in database with provided id"})
     
-        if (!tutor.subjectHourlyRate)
-            return res.status(500).send({ message: "Tutor subjectHourlyRate array does not exist" })
-        const scheduledSubjectHourlyRate = tutor.subjectHourlyRate.find(subject => subject.course == req.body.scheduledSubject)
-        if (!scheduledSubjectHourlyRate)
-            return res.status(500).send({ message: "Unable to find hourly rate associated with subject" })
+        if (tutor.subjectHourlyRate && tutor.subjectHourlyRate.length !== 0) {
+            const scheduledSubjectHourlyRate = tutor.subjectHourlyRate.find(subject => subject.course == req.body.scheduledSubject)
+            if (!scheduledSubjectHourlyRate)
+                return res.status(500).send({ message: "Unable to find hourly rate associated with subject" })
+        
+            tutee.recommendationWeights.budget += (
+                scheduledSubjectHourlyRate.hourlyRate - 
+                tutee.recommendationWeights.budget) * 
+                (scheduledSubjectHourlyRate.hourlyRate > tutee.recommendationWeights.budget ? 0.5 : 0.1)
+        }
     
-        tutee.recommendationWeights.budget += (
-            scheduledSubjectHourlyRate.hourlyRate - 
-            tutee.recommendationWeights.budget) * 
-            (scheduledSubjectHourlyRate.hourlyRate > tutee.recommendationWeights.budget ? 0.5 : 0.1)
+        if (tutor.overallRating)
+            tutee.recommendationWeights.minRating -= (tutee.recommendationWeights.minRating - tutor.overallRating) *
+                (tutor.overallRating < tutee.recommendationWeights.minRating ? 0.5 : 0.1)
     
-        if (tutor.rating)
-            tutee.recommendationWeights.minRating -= (tutee.recommendationWeights.minRating - tutor.rating) *
-                (tutor.rating < tutee.recommendationWeights.minRating ? 0.5 : 0.1)
-
         tutee.recommendationWeights.locationModeWeight -= (tutor.locationMode != tutee.locationMode ?
             0.1 :
             -0.0
@@ -151,10 +113,7 @@ exports.scheduledAppointment = async (req, res) => {
                 (distance > tutee.recommendationWeights.maxDistance ? 0.4 : 0.1)
         }
     
-        await tutee.save().catch(err => {
-            console.log(err)
-            return res.status(500).send({ message: err.message })
-        })
+        await tutee.save()
         return res.status(200).send({ message: "Adjusted weights based on scheduled appointment"})
     } catch (err) {
         console.log(err)
@@ -165,34 +124,24 @@ exports.scheduledAppointment = async (req, res) => {
 // ChatGPT usage: No
 exports.reviewedTutor = async (req, res) => {
     try {
-        if (!mongoose.Types.ObjectId.isValid(req.body.tutorId)) {
-            return res.status(400).send({ message: "Invalid provided tutorId" })
-        }
-        const tutor = await User.findById(req.body.tutorId).catch(err => {
-            console.log(err)
-            return res.status(500).send({ message: err.message })
-        })
-        const tutee = await User.findById(req.userId).catch(err => {
-            console.log(err)
-            return res.status(500).send({ message: err.message })
-        })
+        const tutor = await User.findById(req.body.tutorId)
+        const tutee = await User.findById(req.userId)
+
         if (!tutor || tutor.isBanned)
             return res.status(404).send({ message: "Could not find tutor in database with provided id"})
-        if (!tutee || tutee.isBanned)
-            return res.status(404).send({ message: "Could not find tutee in database with provided id"})
-    
+
         const reviewFactor = req.body.review * 0.1
-    
-        if (!tutor.subjectHourlyRate || tutor.subjectHourlyRate.length !== 0) {
+
+        if (tutor.subjectHourlyRate && tutor.subjectHourlyRate.length !== 0) {
             const averageHourlyRate = tutor.subjectHourlyRate.reduce((acc, subject) => 
                 acc + subject.hourlyRate, 0) / tutor.subjectHourlyRate.length
             tutee.recommendationWeights.budget += (averageHourlyRate - tutee.recommendationWeights.budget) *
                 (averageHourlyRate > tutee.recommendationWeights.budget ? 0.15 : 0.05) * reviewFactor
         }
-    
-        if (tutor.rating)
-            tutee.recommendationWeights.minRating -= (tutee.recommendationWeights.minRating - tutor.rating) *
-                (tutor.rating < tutee.recommendationWeights.minRating ? 0.3 : 0.05) * reviewFactor
+
+        if (tutor.overallRating)
+            tutee.recommendationWeights.minRating -= (tutee.recommendationWeights.minRating - tutor.overallRating) *
+                (tutor.overallRating < tutee.recommendationWeights.minRating ? 0.3 : 0.05) * reviewFactor
 
         tutee.recommendationWeights.locationModeWeight -= (tutor.locationMode != tutee.locationMode ?
             0.05 :
@@ -205,14 +154,11 @@ exports.reviewedTutor = async (req, res) => {
             tutee.recommendationWeights.maxDistance += (distance - tutee.recommendationWeights.maxDistance) *
                 (distance > tutee.recommendationWeights.maxDistance ? 0.2 : 0.02) * reviewFactor
         }
-    
-        await tutee.save().catch(err => {
-            console.log(err)
-            return res.status(500).send({ message: err.message })
-        })
+
+        await tutee.save()
         return res.status(200).send({ message: "Adjusted weights based on reviewed tutor"})
     } catch (err) {
         console.log(err)
         return res.status(500).send({ message: err.message })
-    } 
+    }
 }
---
+++ backend/tests/controllers/recommendation/checkedProfile.test.js
@@ -0,0 +1,326 @@
+const request = require('supertest')
+const mongoose = require("mongoose")
+const db = require("../../../db")
+const { authJwt, account } = require("../../../middleware")
+const { app } = require('../../utils/express.mock.utils')
+const { UserType } = require('../../../constants/user.types')
+const { LocationMode } = require('../../../constants/location.modes')
+const { DEFAULT_RECOMMENDATION_WEIGHTS } = require('../../../controllers/auth.controller')
+
+const ORIGINAL_RECOMMENDATION_WEIGHTS = {
+    budget: 50,
+    minRating: 3,
+    locationModeWeight: 0.5,
+    maxDistance: 20
+}
+
+const ENDPOINT = "/user_action/checked_profile"
+
+
+var mockErrorMsg
+var mockAddedUsers = []
+
+// ChatGPT Usage: No
+jest.mock('../../../db', () => {
+    const originalDb = jest.requireActual('../../../db')
+    class MockUser extends originalDb.user {
+        static findById = jest.fn((id) => {
+            if (mockErrorMsg) {
+                console.log(mockErrorMsg)
+                return Promise.reject(new Error(mockErrorMsg))
+            }
+            return Promise.resolve(mockAddedUsers.find(user => user._id == id))
+        })
+    }
+    return {
+        user: MockUser,
+    }
+})
+
+jest.mock("../../../middleware")
+
+beforeEach(() => {
+    mockErrorMsg = undefined
+    mockAddedUsers = []
+})
+
+const User = db.user
+
+// Interface POST https://edumatch.canadacentral.cloudapp.azure.com/user_action/checked_profile
+describe("Checked profile", () => {
+    const mockTuteeId = new mongoose.Types.ObjectId()
+    const mockTutorId = new mongoose.Types.ObjectId()
+    
+    var mockTutee = {
+        _id: mockTuteeId,
+        googleId: null,
+        isBanned: false,
+        googleOauth: null,
+        type: UserType.TUTEE,
+        username: "testuser",
+        password: "blablablaba",
+        email: "testemail@test.com",
+        displayedName: "Test user",
+        phoneNumber: "123-456-789",
+        education: {
+            school: "test school",
+            program: "test program",
+            level: 1,
+            courses: ["Course 1"],
+        },
+        manualAvailability: [
+            {
+                day: "Sunday",
+                startTime: "09:00",
+                endTime: "17:00"
+            }
+        ],
+        locationMode: LocationMode.IN_PERSON,
+        location: {
+            lat: 123,
+            long: 456
+        },
+        recommendationWeights: DEFAULT_RECOMMENDATION_WEIGHTS,
+        bio: "test bio",
+        useGoogleCalendar: false,
+        userReviews: [],
+        overallRating: 0,
+        appointments: [{
+            _id: "test appointment id",
+            pstStartDateTime: "test date time",
+            pstEndDateTime: "test end date time"
+        }],
+        hasSignedUp: true,
+        save: async() => {
+            return
+        }
+    }
+    
+    var mockTutor = {
+        _id: mockTutorId,
+        googleId: null,
+        isBanned: false,
+        googleOauth: null,
+        type: UserType.TUTOR,
+        username: "testuser2",
+        password: "blablablaba2",
+        email: "testemail2@test.com",
+        displayedName: "Test user2",
+        phoneNumber: "123-456-789",
+        education: {
+            school: "test school",
+            program: "test program",
+            level: 1,
+            courses: ["Course 1"],
+        },
+        manualAvailability: [
+            {
+                day: "Sunday",
+                startTime: "09:00",
+                endTime: "17:00"
+            }
+        ],
+        locationMode: LocationMode.IN_PERSON,
+        location: {
+            lat: 135,
+            long: 479
+        },
+        recommendationWeights: DEFAULT_RECOMMENDATION_WEIGHTS,
+        bio: "test bio",
+        useGoogleCalendar: false,
+        userReviews: [],
+        overallRating: 2,
+        appointments: [{
+            _id: "test appointment id",
+            pstStartDateTime: "test date time",
+            pstEndDateTime: "test end date time"
+        }],
+        hasSignedUp: true
+    }
+
+    authJwt.verifyJwt.mockImplementation((req, res, next) => {
+        req.userId = mockTuteeId
+        return next()
+    })
+
+    // User is not banned
+    account.verifyAccountStatus.mockImplementation((req, res, next) => {
+        return next()
+    })
+
+    // ChatGPT Usage: No
+    // Input: 
+    //  (1) tutorId is valid, exists, and not banned, with locationMode == in person and valid location coordinates
+    //  (2) userId is valid, exists, and not banned, with locationMode == in person and valid location coordinates
+    // Expected status code: 200
+    // Expected behavior: tutee’s minRating, locationModeWeight, maxDistance in recommendationWeights are updated correctly
+    // Expected output: Success message
+    test('Should pass when ids, locationMode, recommendationWeights are valid', async() => {
+        mockAddedUsers.push(mockTutee)
+        mockAddedUsers.push(mockTutor)
+
+        const mockRequestBody = {
+            tutorId: mockTutorId
+        }
+
+        const res = await request(app)
+            .post(ENDPOINT)
+            .set('Authorization', 'Bearer mockToken')
+            .send(mockRequestBody);
+
+        expect(res.status).toBe(200)
+        expect(res.body).toEqual({ 
+            message: "Adjusted weights based on checked profile"
+        })
+        expect(mockAddedUsers.find(user => user._id == mockTuteeId).recommendationWeights).not.toBe(ORIGINAL_RECOMMENDATION_WEIGHTS)
+    })
+
+    // ChatGPT Usage: No
+    // Input: 
+    //  (1) db throws an error upon query
+    // Expected status code: 500
+    // Expected behavior: Database is unchanged
+    // Expected output: Error message
+    test('Should fail when tutorId query throws error', async() => {
+        mockAddedUsers.push(mockTutee)
+        mockAddedUsers.push(mockTutor)
+
+        const mockRequestBody = {
+            tutorId: mockTutorId
+        }
+
+        mockErrorMsg = "Error in finding tutor"
+
+        const res = await request(app)
+            .post(ENDPOINT)
+            .set('Authorization', 'Bearer mockToken')
+            .send(mockRequestBody);
+
+        expect(res.status).toBe(500)        
+        expect(res.body).toEqual({ 
+            message: mockErrorMsg
+        })
+        expect(mockAddedUsers.find(user => user._id == mockTuteeId).recommendationWeights).toBe(DEFAULT_RECOMMENDATION_WEIGHTS)
+    }) 
+
+    // ChatGPT Usage: No
+    // Input: 
+    //  (1) tutorId is valid, exists, and banned
+    // Expected status code: 404
+    // Expected behavior: Database is unchanged
+    // Expected output: Error message
+    test('Should fail when tutor is banned', async() => {
+        mockAddedUsers.push(mockTutee)
+        mockAddedUsers.push({
+           ...mockTutor,
+           isBanned: true
+        })
+
+        const mockRequestBody = {
+            tutorId: mockTutorId
+        }
+
+        const res = await request(app)
+            .post(ENDPOINT)
+            .set('Authorization', 'Bearer mockToken')
+            .send(mockRequestBody);
+
+        expect(res.status).toBe(404)   
+        expect(res.body).toEqual({ 
+            message: "Could not find tutor in database with provided id"
+        })     
+        expect(mockAddedUsers.find(user => user._id == mockTuteeId).recommendationWeights).toBe(DEFAULT_RECOMMENDATION_WEIGHTS)
+    })
+
+    // ChatGPT Usage: No
+    // Input: 
+    //  (1) tutorId for a tutor with missing fields
+    // Expected status code: 200
+    // Expected behavior: Database is unchanged
+    // Expected output: Success message
+    test('Should pass when tutor fields are missing', async() => {
+        mockAddedUsers.push(mockTutee)
+        mockAddedUsers.push({
+            ...mockTutor,
+            overallRating: undefined,
+            location: undefined
+        })
+
+        const mockRequestBody = {
+            tutorId: mockTutorId
+        }
+
+        const res = await request(app)
+            .post(ENDPOINT)
+            .set('Authorization', 'Bearer mockToken')
+            .send(mockRequestBody);
+
+        expect(res.status).toBe(200)   
+        expect(res.body).toEqual({ 
+            message: "Adjusted weights based on checked profile"
+        })     
+        expect(mockAddedUsers.find(user => user._id == mockTuteeId).recommendationWeights).toBe(DEFAULT_RECOMMENDATION_WEIGHTS)
+    })
+
+    // ChatGPT Usage: No
+    // Input: 
+    //  (1) tutorId for a tutor with opposing values to the tutee's recommendationWeights
+    // Expected status code: 200
+    // Expected behavior: tutee’s recommendationWeights are updated correctly
+    // Expected output: Success message
+    test('Should pass when tutor has opposing values to tutee', async() => {
+        mockAddedUsers.push(mockTutee)
+        mockAddedUsers.push({
+            ...mockTutor,
+            overallRating: 5,
+            location: {
+                lat: 123,
+                long: 456
+            }
+        })
+
+        const mockRequestBody = {
+            tutorId: mockTutorId
+        }
+
+        const res = await request(app)
+            .post(ENDPOINT)
+            .set('Authorization', 'Bearer mockToken')
+            .send(mockRequestBody);
+
+        expect(res.status).toBe(200)   
+        expect(res.body).toEqual({ 
+            message: "Adjusted weights based on checked profile"
+        })     
+        expect(mockAddedUsers.find(user => user._id == mockTuteeId).recommendationWeights).not.toBe(ORIGINAL_RECOMMENDATION_WEIGHTS)
+    })
+
+    // ChatGPT Usage: No
+    // Input: 
+    //  (1) tutorId for a tutor who has different locationMode
+    // Expected status code: 200
+    // Expected behavior: tutee’s recommendationWeights are updated correctly
+    // Expected output: Success message
+    test('Should pass when tutor has different locationMode', async() => {
+        mockAddedUsers.push(mockTutee)
+        mockAddedUsers.push({
+            ...mockTutor,
+            locationMode: LocationMode.ONLINE
+        })
+
+        const mockRequestBody = {
+            tutorId: mockTutorId
+        }
+
+        const res = await request(app)
+            .post(ENDPOINT)
+            .set('Authorization', 'Bearer mockToken')
+            .send(mockRequestBody);
+
+        expect(res.status).toBe(200)   
+        expect(res.body).toEqual({ 
+            message: "Adjusted weights based on checked profile"
+        })     
+        expect(mockAddedUsers.find(user => user._id == mockTuteeId).recommendationWeights).not.toBe(ORIGINAL_RECOMMENDATION_WEIGHTS)
+    })
+})

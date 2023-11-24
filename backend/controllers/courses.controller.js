8a1db3a9151dd6f7e3506e3e83f030487fb38838
aryaphan <aryaphan31@gmail.com>
2023-11-24 15:00:59 -0800

    return courses based on prefix

--- backend/controllers/courses.controller.js
+++ backend/controllers/courses.controller.js
@@ -4,14 +4,27 @@ const API_URL =  "https://ubcgrades.com/api/v3"
 
 // ChatGPT usage: No
 exports.getCourseCodes = async (req, res) => {
-    const code = req.query.code
+    var code = req.query.code
     if (!code) {
         return res.status(400).send({ message: "code is required" })
+    }
+    var courseCode
+    if (code.length > 4) {
+        courseCode = code.slice(4).trim()
+        code = code.slice(0, 4)
     }
     axios.get(`${API_URL}/courses/UBCV/${code}`)
             .then((result) => {
                 var data = result.data
-                data = data.map(info => `${code} ${info.course}`)
+                if (courseCode) {
+                    data = data.filter(info => info.course.startsWith(courseCode))
+                }
+                data = data.map(info => {
+                    return {
+                        code: `${code} ${info.course}`,
+                        title: info.course_title
+                    }
+                })
                 var ret = {
                     courses: data
                 }

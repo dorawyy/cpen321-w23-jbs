eb2f230a48fbbc3067aeab681fecacd46aa612ae
lightning7771 <marydingwong@gmail.com>
2023-11-25 15:27:16 -0800

    Some minor FE fixes from TA feedback

--- frontend/EduMatch/app/src/main/java/com/example/edumatch/views/CustomAutoCompleteView.java
+++ frontend/EduMatch/app/src/main/java/com/example/edumatch/views/CustomAutoCompleteView.java
@@ -2,82 +2,71 @@ package com.example.edumatch.views;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
-import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
 
 import com.example.edumatch.R;
 
-import java.util.Arrays;
-
 public class CustomAutoCompleteView extends RelativeLayout {
     private AutoCompleteTextView autoCompleteTextView;
-    private String[] suggestionsArray;
 
-    // ChatGPT usage: Yes
     public CustomAutoCompleteView(Context context) {
         super(context);
         init(context);
     }
 
-    // ChatGPT usage: Yes
     public CustomAutoCompleteView(Context context, AttributeSet attrs) {
         super(context, attrs);
         init(context);
     }
 
-    // ChatGPT usage: Yes
     public CustomAutoCompleteView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init(context);
     }
 
-    // ChatGPT usage: Yes
     public AutoCompleteTextView getAutoCompleteTextView() {
         return autoCompleteTextView;
     }
 
-    // ChatGPT usage: Yes
-    public void setSuggestions(String[] suggestions) {
-        this.suggestionsArray = suggestions;
-
+    public void setThreshold(int threshold) {
         if (autoCompleteTextView != null) {
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_dropdown_item_1line, suggestions);
+            autoCompleteTextView.setThreshold(threshold);
+        }
+    }
+
+    public void setAdapter(ArrayAdapter<String> adapter) {
+        if (autoCompleteTextView != null) {
             autoCompleteTextView.setAdapter(adapter);
         }
     }
 
-    // ChatGPT usage: Yes
+    public void setOnItemClickListener(AdapterView.OnItemClickListener listener) {
+        if (autoCompleteTextView != null) {
+            // Pass the click event to the provided listener
+            // Clear the entered text if it's invalid
+            autoCompleteTextView.setOnItemClickListener(listener);
+        }
+    }
+
+    public void setText(String text) {
+        if (autoCompleteTextView != null) {
+            autoCompleteTextView.setText(text);
+        }
+    }
+
     private void init(Context context) {
         LayoutInflater inflater = LayoutInflater.from(context);
         inflater.inflate(R.layout.auto_complete, this, true);
 
         autoCompleteTextView = findViewById(R.id.auto_complete);
 
-        if (suggestionsArray != null) {
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_dropdown_item_1line, suggestionsArray);
-            autoCompleteTextView.setAdapter(adapter);
-        }
-
         autoCompleteTextView.setOnEditorActionListener((textView, actionId, keyEvent) -> {
-            if (actionId == EditorInfo.IME_ACTION_DONE ||
-                    (keyEvent != null && keyEvent.getKeyCode() == KeyEvent.KEYCODE_ENTER && keyEvent.getAction() == KeyEvent.ACTION_DOWN)) {
-                String enteredText = textView.getText().toString();
-                if (suggestionsArray != null && !Arrays.asList(suggestionsArray).contains(enteredText)) {
-                    // Display an error if the entered text doesn't match any suggestion.
-                    Toast.makeText(context, "Invalid selection", Toast.LENGTH_SHORT).show();
-
-                    // Clear the entered text if it's invalid
-                    textView.setText("");
-                    return true; // Consume the event to prevent further action.
-                }
-            }
             return false; // Let the system perform the default action for the "Done" action.
         });
-
     }
+
 }
